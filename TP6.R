304fb9f3eb318fe8e4f374a4e230b3410b2a5851
Mustapha <sarrboruto18@gmail.com>
2026-04-01 18:21:49 +0000

    fin pour aujour d'hui

--- TP6.R
+++ TP6.R
@@ -31,3 +31,24 @@ summary(model3)
 #Selection de la meilleure variable
 
 step(model3)
+
+#Chargement du package
+library(DAAG)
+#chargement de la base 
+data("litters")
+head(litters)
+
+#Analyse 
+#Modélisation 
+model=lm(brainwt~.,data = litters)
+best=step(model)
+best$anova
+base = data.frame(Rend=c(12,14,10,16,14,19,21,19,21,16,19,21,25,21),Dose=c(2,1,3,6,7,8,8,5,5,8,4,9,12,7),Temp=c(45,43,43,47,42,41,32,33,41,38,32,31,35,29),Eng=c(121,132,154,145,129,156,132,147,128,163,161,172,174,180))
+base
+model4=lm(Rend~.,data = base)
+best=step(model4)
+
+predict(model4,newdata = data.frame(Dose=0.75,Temp=-0.4,Eng=-0.03))
+
+model5=lm(Rend~Dose+Temp,data = base)
+best=step(model5)
